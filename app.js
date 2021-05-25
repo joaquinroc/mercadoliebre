0f8071af75fc3a048bc4a1f29d99e4289b5248ff
joaquinroc <joaquin.ro.canepa@hotmail.com>
2021-05-25 18:52:27 -0300

    subiendo1

---
+++ app.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+app.use(express.static(path.resolve(__dirname,"./public")));
+app.listen(3000,() => {
+    console.log("Servidor corriendo en http://localhost:3000")
+})
+
+app.get("/", (req,res)=> res.sendFile(path.resolve(__dirname,"./views","home.html")))
+
+app.get("/crearCuenta", (req,res) => res.sendFile(path.resolve(__dirname,"./views","crearCuenta.html")))
+
+app.get("/login", (req,res)=> res.sendFile(path.resolve(__dirname,"./views","login.html")))
